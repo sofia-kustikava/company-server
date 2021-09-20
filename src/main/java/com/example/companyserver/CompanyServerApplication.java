3a3c460ed84d0e28186d32c2d23c1b88288d6427
marcosatan666 <81553793+sofia-kustikava@users.noreply.github.com>
2021-09-20 21:04:17 +0300

    Starting project

---
+++ src/main/java/com/example/companyserver/CompanyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.companyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/companyserver/CompanyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
