bcfee378a0b1ee45f09b21cb63bab94d85071cf4
Sofiya Kustsikava <Sofiya.Kustsikava@itechart-group.com>
2021-09-21 15:48:58 +0300

    Created basic entities

--- src/main/java/com/example/companyserver/entity/SubscriptionsEntity.java
+++ src/main/java/com/example/companyserver/entity/SubscriptionsEntity.java
@@ -1,2 +1,30 @@
-package com.example.companyserver.entity;public class SubscriptionsEntity {
+package com.example.companyserver.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "subscriptions")
+public class SubscriptionsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "subscription")
+    private String subscription;
+
+    @Column(name = "date_start")
+    private Date dateStart;
+
+    @Column(name = "date_end")
+    private Date dateEnd;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "users")
+    private List<UsersEntity> users;
 }
--- src/main/java/com/example/companyserver/entity/UsersEntity.java
+++ src/main/java/com/example/companyserver/entity/UsersEntity.java
@@ -1,2 +1,56 @@
-package com.example.companyserver.entity;public class UserEntity {
+package com.example.companyserver.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UsersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private  String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @CreatedDate
+    @Column(name = "date_created")
+    private Date date_created;
+
+    @LastModifiedDate
+    @Column(name = "date_updated")
+    private Date updated;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+    @Column(name = "password")
+    private String passwordHash;
+
+    @ManyToMany( cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "users_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id", referencedColumnName = "id")
+    )
+    private List<RolesEntity> roles = new ArrayList<>();
+
+    @ManyToOne
+    private SubscriptionsEntity subscriptions;
 }
