7cc6489963a42f3c4cc0d5705145c9a93c3614c1
Sofiya.Kustsikava <Sofiya.Kustsikava@itechart-group.com>
2021-10-22 14:58:23 +0300

    added test files

---
+++ src/test/java/com/example/companyserver/service/AuthServiceTest.java
@@ -0,0 +1,19 @@
+package com.example.companyserver.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    public void test() throws Exception {
+
+    }}
