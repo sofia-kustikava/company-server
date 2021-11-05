3a47aea020249f77a721356c009ae92264a923f3
Sofiya.Kustsikava <Sofiya.Kustsikava@itechart-group.com>
2021-11-05 15:56:15 +0300

    Fixed test

--- src/test/java/com/example/companyserver/service/PayPalServiceTest.java
+++ src/test/java/com/example/companyserver/service/PayPalServiceTest.java
@@ -2,14 +2,17 @@ package com.example.companyserver.service;
 
 import com.paypal.api.payments.*;
 import com.paypal.base.rest.PayPalRESTException;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PayPalServiceTest {
 
