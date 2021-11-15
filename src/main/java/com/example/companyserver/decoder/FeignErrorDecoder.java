5b7bee3ca679073e299f4844832cf6d45a2db22f
Sofiya.Kustsikava <Sofiya.Kustsikava@itechart-group.com>
2021-11-15 12:16:06 +0300

    Added Error Decoder for Feign

--- src/main/java/com/example/companyserver/decoder/FeignErrorDecoder.java
+++ src/main/java/com/example/companyserver/decoder/FeignErrorDecoder.java
@@ -1,19 +1,33 @@
 package com.example.companyserver.decoder;
 
+import com.example.companyserver.exceptions.BadRequestException;
+import com.example.companyserver.exceptions.ForbiddenException;
+import com.example.companyserver.exceptions.NotFoundException;
+import com.example.companyserver.exceptions.ServerErrorException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class FeignErrorDecoder implements ErrorDecoder {
-    private final ErrorDecoder defaultErrorDecoder = new Default();
 
     @Override
-    public Exception decode(String s, Response response) {
-        System.out.println("Error Response!!!");
+    public Exception decode(String methodKey, Response response) {
 
-        if (400 == response.status()) {
-            System.out.println("It's a 400 Error!!!");
+        switch (response.status()) {
+            case 400:
+                return new BadRequestException(response.reason());
+            case 403:
+                return new ForbiddenException(response.reason());
+            case 404: {
+                return new NotFoundException(response.reason());
+            }
+            case 500:
+                return new ServerErrorException(response.reason());
+            default:
+                return new Exception(response.reason());
         }
-
-        return defaultErrorDecoder.decode(s, response);
     }
+
 }
