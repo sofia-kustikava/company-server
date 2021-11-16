242a4995cd8a519c1d8cafa21aca5294282855da
Sofiya.Kustsikava <Sofiya.Kustsikava@itechart-group.com>
2021-11-16 14:19:16 +0300

    That's really it

--- src/main/java/com/example/companyserver/service/SubscriptionService.java
+++ src/main/java/com/example/companyserver/service/SubscriptionService.java
@@ -56,7 +56,7 @@ public class SubscriptionService {
 
     public String paymentForSubscription() throws PayPalRESTException {
         UserEntity user = authenticationService.getUser();
-        if (user.getSubscription() != null && user.getSubscription().getSubscriptionStatus().equals(SubscriptionStatus.INACTIVE)) {
+        if (user.getSubscription() != null && !user.getSubscription().getSubscriptionStatus().equals(SubscriptionStatus.ACTIVE)) {
             Payment payment = payPalService.createPayment(
                     user.getId(),
                     user.getSubscription().getSubscription().getPrice(),
