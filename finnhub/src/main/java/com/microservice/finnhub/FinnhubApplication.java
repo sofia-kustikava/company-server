40b53eb753736e66054f0e464b9d9470e2bb09b9
Sofiya.Kustsikava <Sofiya.Kustsikava@itechart-group.com>
2021-11-18 12:24:13 +0300

    Created new directory

---
+++ finnhub/src/main/java/com/microservice/finnhub/FinnhubApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.finnhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinnhubApplication.class, args);
+    }
+
+}
